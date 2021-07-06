be90ba1795d674f4b708916713eff960c4b3fcdf
Manfrinne <m4nfrinne@gmail.com>
2021-07-05 22:09:35 -0300

    chore: Add itens in the Cart;

--- README.md
+++ README.md
@@ -80,6 +80,54 @@ Nesse módulo vamos refatorar o código utilizando o padrão MVC, além disso, v
 
 - [x] - Animações com Lottie e Mensagens pós Pedido
 
+### Funcionalidades Extras para a Launchstore:
+
+- [x] - Estrutura do Carrinho
+
+- [x] - Adicionando Itens ao Carrinho
+
+- [ ] - Removendo Item do Carrinho
+
+- [ ] - Deletando Itens do Carrinho
+
+- [ ] - Estrutura HTML do Carrinho
+
+- [ ] - Estilizando o Carrinho
+
+- [ ] - Utilizando a Funcionalidade de Adicionar ao Carrinho
+
+- [ ] - Utilizando a Funcionalidade Remover 1 item do Carrinho
+
+- [ ] - Carrinho Vazio
+
+- [ ] - Adicionar o Carrinho ao Menu
+
+- [ ] - Tabela de Pedidos
+
+- [ ] - Criar Pedidos
+
+- [ ] - ados do Pedido
+
+- [ ] - Estrutura HTML dos Meus Pedidos
+
+- [ ] - Estilo da Página Meus Pedidos
+
+- [ ] - Service de Pedidos
+
+- [ ] - Minhas Vendas e Detalhes do Pedido
+
+- [ ] - Estilo da página Detalhes do Pedido
+
+- [ ] - Menu para Editar Pedido
+
+- [ ] - Aplicar Dropdown no Menu Principal
+
+- [ ] - Mudar Status do Pedido
+
+- [ ] - Estratégia de Soft Delete no Banco de Dados
+
+- [ ] - Soft Delete na Aplicação
+
 
 ## **Como contribuir**
 
--- src/lib/cart.js
+++ src/lib/cart.js
@@ -17,9 +17,69 @@ const Cart = {
     return this
   },
 
-  addOne(product){},
+  addOne(product) {
+
+    let inCart = this.items.find(item => item.product.id == product.id)
+
+    if (!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price)
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      }
+
+      this.items.push(inCart)
+    }
+
+    if (inCart.quantity >= product.quantity) return this
+
+    inCart.quantity++
+    inCart.price = inCart.product.price * inCart.quantity
+    inCart.formattedPrice = formatPrice(inCart.price)
+
+    this.total.quantity++
+    this.total.price += inCart.product.price
+    this.total.formattedPrice = formatPrice(this.total.price)
+
+    return this
+  },
+
   removeOne(productId){},
+
   delete(productId){}
 }
 
-console.log(Cart)
+const product = {
+  id: 1,
+  price: 199,
+  quantity: 2
+}
+
+const product2 = {
+  id: 2,
+  price: 400,
+  quantity: 1
+}
+
+
+console.log("ADD FIRST CART ITEM")
+let oldCart = Cart.init().addOne(product)
+console.log(oldCart)
+
+console.log("ADD SECOND CART ITEM")
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
+
+console.log("ADD THIRD CART ITEM")
+oldCart = Cart.init(oldCart).addOne(product2)
+console.log(oldCart)
+
+console.log("ADD LAST CART ITEM")
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
+
+module.exports = Cart
